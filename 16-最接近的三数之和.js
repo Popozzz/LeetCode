8545d6cf343f782afd9d054e1b05520bf7bcde02
popo <p1whoh8j@qq.com>
2020-06-24 20:55:56 +0800

    每日一题打卡16

---
+++ 16-最接近的三数之和.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    for (var i=0; i<nums.length; i++) {
+        for (var j=i+1; j<nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                let t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+        }
+    }
+
+    var start = 0, i = 1, end = nums.length - 1, min;
+    
+    for (var i=1; i<nums.length-1; i++) {
+        start = 0;
+        end = nums.length - 1;
+        while(i> start && start < end && i < end) {
+            let sum = nums[start] + nums[i] + nums[end];
+            let minus = sum - target;
+            if (min == null || Math.abs(minus) < Math.abs(min)) {
+                min = minus;
+            }
+
+            if (sum > target) {
+                end --;
+            } else {
+                start ++;
+            }
+        }
+    }
+
+    return target + min;
+};
